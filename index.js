4006221d98baff43ecab7851541c5cdc516648d8
Toohina Barua <toohi2000@gmail.com>
2023-10-30 10:52:19 +0530

    Starting with npm

---
+++ index.js
@@ -0,0 +1,7 @@
+import generateName from 'sillyname';
+import superheroes from 'superheroes';
+
+var sillyName = generateName();
+
+console.log(`Hi, my name is ${sillyName}`);
+console.log(`I am ${superheroes.random()}`);
